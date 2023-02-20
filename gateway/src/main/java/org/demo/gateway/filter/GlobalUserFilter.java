15ee46587d79bfc08f3f010e41fe842c3fd92656
lzy <lha602@163.com>
2023-02-20 19:56:03 +0800

    网关服务

---
+++ gateway/src/main/java/org/demo/gateway/filter/GlobalUserFilter.java
@@ -0,0 +1,36 @@
+package org.demo.gateway.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * 用户请求全局过滤器
+ * @author lzy
+ */
+@Slf4j
+@Component
+public class GlobalUserFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        ServerHttpRequest request = exchange.getRequest();
+        String sourceAddress = request.getLocalAddress().getHostString();
+        log.info("请求来源地址ip " + sourceAddress);
+        log.info("请求来源地址 " + request.getRemoteAddress());
+        log.info("请求类型 " + request.getMethod());
+        log.info("请求地址 " + request.getPath());
+        log.info("请求参数 " + request.getQueryParams());
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return 0;
+    }
+}
